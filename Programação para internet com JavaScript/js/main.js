ca5a409c7d24fa0ecdee188a7affb6a99b44e628
Lucas Nabesima <lucas.nabesima@gmail.com>
2021-03-20 18:51:16 -0300

    Unidade 1, completa

---
+++ Programação para internet com JavaScript/js/main.js
@@ -0,0 +1,47 @@
+/*
+var nome = "Lucas Nabesima";
+var idade1 = 31;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo!";
+
+console.log(nome);
+console.log(idade1 + idade2);
+console.log(frase.replace("Japão", "Brasil"))
+*/
+
+/*
+var lista = ["maçã", "pera", "laranja"];
+//lista.pop()
+lista.push("uva");
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+/*
+var frutas = [{nome: "Maçã", cor: "vermelha"}, {nome: "Uva", cor: "roxa"}];
+console.log(frutas);
+alert(frutas[0].nome);
+
+var idade = prompt("Informe a sua idade:");
+if(idade >= 18){
+    alert("Maior de idade.")
+}else{
+    alert("Menor de idade!")
+};
+
+var contador = 0;
+while(contador <= 5){
+    console.log(contador);
+    contador++;
+};
+
+var count
+for(count=1; count<=5; count++){
+    console.log(count)
+};
+*/
+var d = new Date;
+alert(d.getDay());
+alert(d.getMonth()+1);
+alert(d.getFullYear());
+alert(d.getDate());
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 2. ~~Lógica de programação essencial~~
 3. ~~Introdução ao Git e ao GitHub~~
 4. ~~Introdução à criação de websites com HTML5 e CSS3~~
-5. Recriando a página inicial do Instagram
+5. ~~Recriando a página inicial do Instagram~~
 6. Programação para internet com JavaScript
 7. Introdução à programação com JavaScript
 8. Recriando a interface do Netflix
