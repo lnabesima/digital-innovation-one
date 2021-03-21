fbb03528f95723ff77bfa116dffafc515ea1c493
Lucas Nabesima <lucas.nabesima@gmail.com>
2021-03-21 19:51:00 -0300

    Unidade 2, concluída.

--- Programação para internet com JavaScript/js/main.js
+++ Programação para internet com JavaScript/js/main.js
@@ -1,63 +1,23 @@
-/*
-var nome = "Lucas Nabesima";
-var idade1 = 31;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo!";
+function carregada(){
+    document.getElementById("loaded").innerHTML = "Página carregada, meus bons!"
+}
 
-console.log(nome);
-console.log(idade1 + idade2);
-console.log(frase.replace("Japão", "Brasil"))
-*/
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>"
+}
 
-/*
-var lista = ["maçã", "pera", "laranja"];
-//lista.pop()
-lista.push("uva");
-console.log(lista);
-console.log(lista.toString());
-console.log(lista.join(" - "));
-*/
-/*
-var frutas = [{nome: "Maçã", cor: "vermelha"}, {nome: "Uva", cor: "roxa"}];
-console.log(frutas);
-alert(frutas[0].nome);
+function redirecionar(){
+    window.open("https://google.com.br")
+}
 
-var idade = prompt("Informe a sua idade:");
-if(idade >= 18){
-    alert("Maior de idade.")
-}else{
-    alert("Menor de idade!")
-};
+function trocartexto(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse!"
+}
 
-var contador = 0;
-while(contador <= 5){
-    console.log(contador);
-    contador++;
-};
+function voltarTexto(elemento){
+    elemento.innerHTML= "Passe o mouse aqui!"
+}
 
-var count
-for(count=1; count<=5; count++){
-    console.log(count)
-};
-
-var d = new Date;
-alert(d.getDay());
-alert(d.getMonth()+1);
-alert(d.getFullYear());
-alert(d.getDate());
-
-var trajeto = ["mãe", "floresta", "avó"];
-//console.log(trajeto.push("lobo"));
-//console.log(trajeto.pop());
-console.log(trajeto.join(" > "));
-//console.log(trajeto[1]);
-//console.log(trajeto.toString());
-console.log(trajeto);
-
-var d = new Date;
-console.log("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
-
-var lista = [“Alemanha”, “Inglaterra”, “Escócia”];
-lista.push(“Polônia”);
-lista.pop(“Inglaterra”);
-*/
+function functionChange(elemento){
+    alert(elemento.value)
+}
--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# digital-innovation-one
-## Exercícios e testes dos Bootcamps da DIO.
-### 1. HTML Web Developer
+# Exercícios e testes dos Bootcamps da DIO.
+## 1. HTML Web Developer
 1. ~~Bem Vindo à DIO~~
 2. ~~Lógica de programação essencial~~
 3. ~~Introdução ao Git e ao GitHub~~
 4. ~~Introdução à criação de websites com HTML5 e CSS3~~
 5. ~~Recriando a página inicial do Instagram~~
-6. Programação para internet com JavaScript
+6. ~~Programação para internet com JavaScript~~
 7. Introdução à programação com JavaScript
 8. Recriando a interface do Netflix
 9. Construindo páginas para internet com Bootstrap
