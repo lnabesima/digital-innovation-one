c2f372acbdad32cc999ef12ed7418ca066656adf
Lucas Nabesima <lucas.nabesima@gmail.com>
2021-03-23 23:06:03 -0300

    Alimentação da cobrinha definida

--- 11. Recriando o jogo da cobrinha com JavaScript/script.js
+++ 11. Recriando o jogo da cobrinha com JavaScript/script.js
@@ -60,7 +60,15 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != comida.x || snakeY != comida.y){
+        snake.pop(); 
+    }
+    else{comida.x = Math.floor(Math.random() * 15 + 1) * box;
+        comida.y = Math.floor(Math.random() * 15 + 1) * box; 
+
+    }
+
+
 
     let newHead = {
         x: snakeX,
